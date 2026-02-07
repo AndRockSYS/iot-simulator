a21f0291a3ddf865d19413535e4f27c0c81fc33e
AndRockSYS <106549955+AndRockSYS@users.noreply.github.com>
2026-02-07 18:32:06 +0100

    fix: added yield refactor: cleaner code

--- src/core/network.py
+++ src/core/network.py
@@ -21,27 +21,27 @@ class Network:
         cls.mailboxes[node.id] = []
 
     @classmethod
-    def broadcast(cls, sender: "Node", message):
+    def broadcast(cls, sender: "Node", message: dict):
         """
         Broadcasts the message from sender to all the nodes in the network
         The message is received only by other nodes, who are in RECEIVE mode
-        Packegt has additional propagration delay as well as chance of being lost
+        Packet has additional propagration delay as well as chance of being lost
         """
-        yield sender.env.timeout(*PROP_DELAY_RANGE)
+        yield sender.env.timeout(random.uniform(*PROP_DELAY_RANGE))
         for node in cls.nodes:
             if node.id == sender.id:
                 continue
-            if node.state != Mode.RECEIVE:
+            if node.mode != Mode.RECEIVE:
                 continue
             if random.random() > PT_LOSS:
-                node.env.process(node.receive(message)) 
+                yield node.env.process(node.receive(message)) 
                 cls.mailboxes[node.id].append(message)
     
     @classmethod
-    def messages_received_from(cls, node: "Node"):
+    def messages_received_from(cls, node: "Node") -> list[int]:
         """
         Returns the ids of all nodes it received the messages from during a specific period
         """
-        ids = [msg['id'] for msg in cls.mailboxes[node.id]]
+        ids = [msg['from'] for msg in cls.mailboxes[node.id]]
         cls.mailboxes[node.id] = []
         return ids
